fd5902046a1f09ff8b850affdc92f3411fc652a7
Christian Vilca <christian.vilcaq@gmail.com>
2021-07-09 16:44:59 -0500

    feat: query methods

--- src/main/java/com/christian/market/persistence/ProductoRepository.java
+++ src/main/java/com/christian/market/persistence/ProductoRepository.java
@@ -1,6 +1,7 @@
 package com.christian.market.persistence;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.christian.market.persistence.crud.ProductoCrudRepository;
 import com.christian.market.persistence.entity.Producto;
@@ -9,6 +10,15 @@ public class ProductoRepository {
   private ProductoCrudRepository productoCrudRepository;
 
   public List<Producto> getAll() {
-    return (List<Producto>)productoCrudRepository.findAll();
+    return (List<Producto>) productoCrudRepository.findAll();
   }
+
+  public List<Producto> getByCategoria(int idCategoria) {
+    return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+  }
+
+  public Optional<List<Producto>> getEscasos(int cantidad) {
+    return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+  }
+
 }
--- src/main/java/com/christian/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/christian/market/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,15 @@
 package com.christian.market.persistence.crud;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.christian.market.persistence.entity.Producto;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+  List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+  Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }

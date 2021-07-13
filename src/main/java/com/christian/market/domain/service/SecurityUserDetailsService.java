73a6dd54080f6599ff870a7942206cb6549c48a5
Christian Vilca <christian.vilcaq@gmail.com>
2021-07-13 12:49:29 -0500

    feat: add Spring boot security

---
+++ src/main/java/com/christian/market/domain/service/SecurityUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.christian.market.domain.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityUserDetailsService implements UserDetailsService {
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return new User("christian", "{noop}123456", new ArrayList<>());
+  }
+}
---
+++ src/main/java/com/christian/market/web/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.christian.market.web.security;
+
+import com.christian.market.domain.service.SecurityUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private SecurityUserDetailsService securityUserDetailsService;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(securityUserDetailsService);
+  }
+
+}

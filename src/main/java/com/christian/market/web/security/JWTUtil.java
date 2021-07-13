d61e80a4a78c09eb0d621e321f004f993b59df22
Christian Vilca <christian.vilcaq@gmail.com>
2021-07-13 16:47:05 -0500

    feat: Authotization JWT

--- src/main/java/com/christian/market/web/security/JWTUtil.java
+++ src/main/java/com/christian/market/web/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -21,5 +22,21 @@ public class JWTUtil {
             .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
             .signWith(SignatureAlgorithm.HS256, KEY)
             .compact();
+  }
+
+  public boolean validateToken(String token, UserDetails userDetails ) {
+    return userDetails.getUsername().equals(extractUserName(token)) && !isTokenExpired(token);
+  }
+
+  public String extractUserName(String token) {
+    return getClaims(token).getSubject();
+  }
+
+  public boolean isTokenExpired(String token) {
+    return getClaims(token).getExpiration().before(new Date());
+  }
+
+  private Claims getClaims(String token) {
+    return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody();
   }
 }

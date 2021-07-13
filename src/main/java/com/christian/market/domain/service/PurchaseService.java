ff8c8a08946880459beb9b1d98523ae272442a2a
Christian Vilca <christian.vilcaq@gmail.com>
2021-07-12 21:54:50 -0500

    feat: add PurchaseService and PurchaseController

---
+++ src/main/java/com/christian/market/domain/service/PurchaseService.java
@@ -0,0 +1,29 @@
+package com.christian.market.domain.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.christian.market.domain.Purchase;
+import com.christian.market.domain.repository.PurchaseRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseService {
+  @Autowired
+  private PurchaseRepository purchaseRepository;
+
+  public List<Purchase> getAll() {
+    return purchaseRepository.getAll();
+  }
+
+  public Optional<List<Purchase>> getByClient(String purchaseId) {
+    return purchaseRepository.getByClient(purchaseId);
+  }
+
+  public Purchase save(Purchase purchase) {
+    return purchaseRepository.save(purchase);
+  }
+
+}
